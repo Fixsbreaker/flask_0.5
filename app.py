8efcc6a204e86c0f06289575f1d629f356f19c7a
vibe-reverb <85639083+vibe-reverb@users.noreply.github.com>
2022-02-23 13:59:26 +0600

    запуск

--- app.py
+++ app.py
@@ -113,3 +113,7 @@ def form_sample():
         print(request.form['accept'])
         print(request.form['sex'])
         return "Форма отправлена"
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='127.0.0.1')
